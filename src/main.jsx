1c62305ab7b3c38b74eada852e106d43c6e6a597
Landy510 <randy.chen@tpisoftware.com>
2023-03-13 10:10:59 +0800

    chore: modify homepage setting 1. modify - homepage setting in package.json 2. change BrowswerRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 import '@/styles/app.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App></App>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
